5b73cb9b5da7d25399640160c7037dc8452a1c30
Ozone <mohammaddr653@gmail.com>
2025-06-05 20:10:08 +0330

    v87-starting to do some styling

--- front-end/src/components/common/header.tsx
+++ front-end/src/components/common/header.tsx
@@ -4,29 +4,31 @@ import useHeaderLog from "../../hooks/logics/useHeaderLog";
 const Header = () => {
   const { user, list, userLogout } = useHeaderLog();
   return (
-    <div className="bg-pink-300">
-      <h1>hello {user?.name} this is header</h1>
-      <nav>
-        <ul ref={list}>{/* dynamic */}</ul>
-      </nav>
-      {!user ? (
-        <>
-          <Link to={"/auth/register"}>register</Link>
-          <br />
-          <Link to={"/auth/login"}>login</Link>
-        </>
-      ) : (
-        <>
-          {user?.isadmin ? <Link to={"/admin"}>پنل ادمین</Link> : null}
-          <br />
-          <Link to={"/user"}>حساب کاربری</Link>
-          <br />
-          <Link to={"/cart"}>سبد خرید</Link>
-          <br />
-          <button onClick={userLogout}>logout</button>
-        </>
-      )}
-    </div>
+    <header>
+      <div id="header-container" className="border-8 border-red-600 flex flex-row justify-between items-center">
+        <div>
+          <nav>
+            <ul ref={list} className="flex flex-row gap-2"></ul>
+          </nav>
+        </div>
+        <div>
+          {!user ? (
+            <>
+              <Link to={"/auth/register"}>register</Link>
+              <br />
+              <Link to={"/auth/login"}>login</Link>
+            </>
+          ) : (
+            <div className="flex flex-row gap-2">
+              {user?.isadmin ? <Link to={"/admin"}>پنل ادمین</Link> : null}
+              <Link to={"/user"}>حساب کاربری</Link>
+              <Link to={"/cart"}>سبد خرید</Link>
+              <button onClick={userLogout}>خروج از حساب</button>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
   );
 };
 
