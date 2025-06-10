09ba6fc39a8aaafa717d8b55d614bce368c0e2c5
Ozone <mohammaddr653@gmail.com>
2025-06-10 22:52:46 +0330

    v100-some modification on hero-card comp

--- front-end/src/components/common/hero-card.tsx
+++ front-end/src/components/common/hero-card.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 const HeroCard = () => {
   return (
-    <div className="w-full flex flex-col items-center gap-4 p-4">
+    <Link to={"/shop"} className="relative h-full w-full flex flex-col items-center gap-4 p-4">
       <img
         src="/images/icons/d0i713732ffa55576e36d0dd32dd1c74068.png"
         width={350}
@@ -9,10 +11,19 @@ const HeroCard = () => {
       <p className="flex flex-row items-center gap-1 text-size24 font-weight400 text-cu-neutral-400 text-shadow-md">
         <span> قلب خانه را زیبا کن</span>
         <span>
-          <img src="/images/icons/icons8-heart-32.png" width={30} alt="" />
+          <img
+            src="/images/icons/icons8-heart-32.png"
+            className="animate-bounce"
+            width={30}
+            alt="heart-icon"
+          />
         </span>
       </p>
-    </div>
+      <p className="flex flex-row items-center gap-1 absolute top-0 left-0 text-size13 p-2 px-4 text-cu-neutral-400">
+        <span>فروشگاه</span>
+        <i className="bi bi-arrow-left"></i>
+      </p>
+    </Link>
   );
 };
 export default HeroCard;
