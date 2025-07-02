62c36a13d19296be559456d75edf3ebd641ff903
Ozone <mohammaddr653@gmail.com>
2025-07-03 00:14:32 +0330

    v136-deployed project to liara.ir

--- front-end/config.ts
+++ front-end/config.ts
@@ -4,7 +4,7 @@ const site_key = import.meta.env.VITE_SITE_KEY;
 
 export const SERVER_URL =
   mode === "production"
-    ? "https://venzo2025-backend.vercel.app"
+    ? "https://venzo2025-backend.liara.run"
     : "http://127.0.0.1:5000";
 
 export const SERVER_API = SERVER_URL + "/api";
