3286f744158975fa448eb3d924d84755a339a0bc
Ozone <mohammaddr653@gmail.com>
2025-08-10 21:16:20 +0330

    v237-created carousel for childCategories component

--- front-end/src/components/common/childCategories.tsx
+++ front-end/src/components/common/childCategories.tsx
@@ -1,27 +1,52 @@
 import { Link } from "react-router-dom";
 import Img from "./img";
+import "../../assets/css/childCategories.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+import SwiperButtonPrev from "./swiper-button-prev";
+import SwiperButtonNext from "./swiper-button-next";
 
 const ChildCategories = ({ childCats, categoryId }: any) => {
   return (
-    <div className="flex flex-row px-5 md:px-20 gap-3">
-      {childCats.map((item: any, index: any) => {
-        if (item.motherId === categoryId) {
-          return (
-            <Link
-              key={index}
-              to={`/shop/${item._id}`}
-              className=" rounded-md w-[150px] flex flex-col gap-2 items-center"
-            >
-              <Img
-                pic={item.img}
-                sizes={"500px"}
-                className={"object-cover w-full border border-neutral-300 flex-1"}
-              ></Img>
-              <h3>{item.name}</h3>
-            </Link>
-          );
-        }
-      })}
+    <div className="childCategories-container px-5 md:px-20">
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={20}
+        initialSlide={0}
+        className="mySwiper"
+      >
+        <div className="swiperjs-controlls">
+          <SwiperButtonPrev></SwiperButtonPrev>
+          <SwiperButtonNext></SwiperButtonNext>
+        </div>
+        {childCats.map((item: any, index: any) => {
+          if (item.motherId === categoryId) {
+            return (
+              <SwiperSlide className="max-w-[200px]">
+                <Link
+                  key={index}
+                  to={`/shop/${item._id}`}
+                  className=" rounded-md w-full flex flex-col justify-between border overflow-hidden border-neutral-300 p-2 gap-2 items-center"
+                >
+                  <Img
+                    pic={item.img}
+                    sizes={"500px"}
+                    className={"object-cover w-full border border-neutral-300"}
+                  ></Img>
+                  <h3 className="text-size15">{item.name}</h3>
+                </Link>
+              </SwiperSlide>
+            );
+          }
+        })}
+      </Swiper>
     </div>
   );
 };
