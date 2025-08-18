21ae25023b47e70fa05fcdb913452153f9460daf
Ozone <mohammaddr653@gmail.com>
2025-08-18 15:37:27 +0330

    v243-fix on items-slideshow

--- front-end/src/components/common/items-slideshow.tsx
+++ front-end/src/components/common/items-slideshow.tsx
@@ -20,92 +20,92 @@ import Img from "./img";
 
 function ItemsSlideshow() {
   return (
-    <div className="relative h-[400px] items-slideshow-container lg:bg-[url(/images/pexels-dada-_design-240566386-17271985.jpg)] lg:border lg:border-neutral-primary bg-cover bg-no-repeat bg-bottom w-full rounded-lg flex items-center">
-      <div className="relative h-[300px] lg:px-10 w-full flex items-center justify-center lg:justify-end">
-        <div className=" w-full lg:w-[500px] h-full absolute">
-          <Swiper
-            className=""
-            modules={[Autoplay, Pagination, Navigation, Scrollbar, A11y]}
-            initialSlide={0}
-            pagination={{
-              clickable: true,
-            }}
-            loop={true}
-            spaceBetween={"10px"}
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-              reverseDirection: true,
-              pauseOnMouseEnter: true,
-            }}
-            breakpoints={{
-              0: {
-                slidesPerView: 1.3,
-              },
-              576: {
-                slidesPerView: 2.5,
-              },
-              768: {
-                slidesPerView: 2.3,
-              },
-              992: {
-                slidesPerView: 2,
-              },
-            }}
-          >
-            {/* note: استایل این دکمه ها رو با تیلویند بده تا جایی که میشه */}
-            <div className="swiperjs-controlls">
-              <SwiperButtonPrev className={"hidden lg:flex"}></SwiperButtonPrev>
-              <SwiperButtonNext className={"hidden lg:flex"}></SwiperButtonNext>
-            </div>
+    <div className="border border-neutral-200 lg:border-0 lg:py-20 lg:px-10 items-slideshow-container lg:bg-[url(/images/pexels-dada-_design-240566386-17271985.jpg)] lg:border lg:border-neutral-primary bg-cover bg-no-repeat bg-bottom w-full rounded-lg flex items-center justify-center lg:justify-end">
+      <div className=" w-full lg:w-[500px] relative">
+        <Swiper
+          className=""
+          modules={[Autoplay, Pagination, Navigation, Scrollbar, A11y]}
+          initialSlide={0}
+          loop={true}
+          spaceBetween={"10px"}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+            reverseDirection: true,
+            pauseOnMouseEnter: true,
+          }}
+          breakpoints={{
+            0: {
+              slidesPerView: 2.3,
+            },
+            576: {
+              slidesPerView: 3.3,
+            },
+            768: {
+              slidesPerView: 4.3,
+            },
+            992: {
+              slidesPerView: 2,
+            },
+          }}
+        >
+          {/* note: استایل این دکمه ها رو با تیلویند بده تا جایی که میشه */}
+          <div className="swiperjs-controlls flex justify-end w-full items-center gap-2 absolute top-full h-[20px] mt-[20px] z-10 text-black">
+            <SwiperButtonPrev
+              className={
+                "hidden cursor-pointer lg:flex w-[30px] bg-white aspect-square justify-center items-center rounded-full"
+              }
+            ></SwiperButtonPrev>
+            <SwiperButtonNext
+              className={
+                "hidden cursor-pointer lg:flex w-[30px] bg-white aspect-square justify-center items-center rounded-full"
+              }
+            ></SwiperButtonNext>
+          </div>
 
-            <SwiperSlide>
-              <div className="item-container p-3 rounded-lg">
-                <div className="bg-green-300 bg-[url(/images/pexels-yasin-thamir-506684371-16152262.jpg)] bg-cover bg-top aspect-square rounded-lg overflow-hidden mx-auto shadow-md shadow-glass-shadow">
-                  <div className="bg-linear-to-t from-[#00000057] to-transparent flex flex-col justify-end h-full">
-                    <div className="flex justify-between p-4 items-center text-white">
-                      <div className="flex flex-col gap-2">
-                        <h4>لامپ</h4>
-                        <p className="text-size13 font-weight100">23 محصول</p>
-                      </div>
-                      <i className="bi bi-arrow-left"></i>
-                    </div>
+          <SwiperSlide>
+            <div className="blur-layer absolute rounded-lg w-full h-full z-0 "></div>
+            <div className="relative w-full bg-green-300 bg-[url(/images/pexels-yasin-thamir-506684371-16152262.jpg)] bg-cover bg-top aspect-square rounded-lg overflow-hidden mx-auto shadow-md shadow-glass-shadow z-10">
+              <div className="bg-linear-to-t from-[#00000057] to-transparent flex flex-col justify-end h-full">
+                <div className="flex justify-between p-4 items-center text-white">
+                  <div className="flex flex-col gap-2">
+                    <h4>لامپ</h4>
+                    <p className="text-size13 font-weight100">23 محصول</p>
                   </div>
+                  <i className="bi bi-arrow-left"></i>
                 </div>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="item-container p-3 rounded-lg">
-                <div className="bg-green-300 bg-[url(/images/pexels-yasin-thamir-506684371-16152262.jpg)] bg-cover bg-top aspect-square rounded-lg overflow-hidden mx-auto shadow-md shadow-glass-shadow">
-                  <div className="bg-linear-to-t from-[#00000057] to-transparent flex flex-col justify-end h-full">
-                    <div className="flex justify-between p-4 items-center text-white">
-                      <div className="flex flex-col gap-2">
-                        <h4>لامپ</h4>
-                        <p className="text-size13 font-weight100">23 محصول</p>
-                      </div>
-                      <i className="bi bi-arrow-left"></i>
-                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="blur-layer absolute rounded-lg w-full h-full z-0"></div>
+            <div className="relative w-full bg-green-300 bg-[url(/images/pexels-yasin-thamir-506684371-16152262.jpg)] bg-cover bg-top aspect-square rounded-lg overflow-hidden mx-auto shadow-md shadow-glass-shadow z-10">
+              <div className="bg-linear-to-t from-[#00000057] to-transparent flex flex-col justify-end h-full">
+                <div className="flex justify-between p-4 items-center text-white">
+                  <div className="flex flex-col gap-2">
+                    <h4>لامپ</h4>
+                    <p className="text-size13 font-weight100">23 محصول</p>
                   </div>
+                  <i className="bi bi-arrow-left"></i>
                 </div>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="item-container p-3 rounded-lg">
-                <div className="bg-green-300 bg-[url(/images/pexels-yasin-thamir-506684371-16152262.jpg)] bg-cover bg-top aspect-square rounded-lg overflow-hidden mx-auto shadow-md shadow-glass-shadow">
-                  <div className="bg-linear-to-t from-[#00000057] to-transparent flex flex-col justify-end h-full">
-                    <div className="flex justify-between p-4 items-center text-white">
-                      <div className="flex flex-col gap-2">
-                        <h4>لامپ</h4>
-                        <p className="text-size13 font-weight100">23 محصول</p>
-                      </div>
-                      <i className="bi bi-arrow-left"></i>
-                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="blur-layer absolute rounded-lg w-full h-full z-0"></div>
+            <div className="relative w-full bg-green-300 bg-[url(/images/pexels-yasin-thamir-506684371-16152262.jpg)] bg-cover bg-top aspect-square rounded-lg overflow-hidden mx-auto shadow-md shadow-glass-shadow z-10">
+              <div className="bg-linear-to-t from-[#00000057] to-transparent flex flex-col justify-end h-full">
+                <div className="flex justify-between p-4 items-center text-white">
+                  <div className="flex flex-col gap-2">
+                    <h4>لامپ</h4>
+                    <p className="text-size13 font-weight100">23 محصول</p>
                   </div>
+                  <i className="bi bi-arrow-left"></i>
                 </div>
               </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
