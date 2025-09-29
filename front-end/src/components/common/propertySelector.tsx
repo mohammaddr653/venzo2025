87535846ea328638e91805a4a74fe2a0707cb785
Ozone <mohammaddr653@gmail.com>
2025-09-29 16:20:53 +0330

    v275-little fix in UI

--- front-end/src/components/common/propertySelector.tsx
+++ front-end/src/components/common/propertySelector.tsx
@@ -7,7 +7,7 @@ const PropertySelector = (props: PropertySelectorProps) => {
   if (props.type === "color") {
     return (
       <span
-        className={`w-8 h-8 flex border-2 rounded-full border-white z-10 inner ${
+        className={`cursor-pointer w-8 h-8 flex border-2 rounded-full border-white z-10 inner ${
           props.formData.selectedPropertyvalString ===
             props.propertyval.propertyval._id &&
           "border-2 border-white outline-2 outline-green-600"
@@ -20,7 +20,13 @@ const PropertySelector = (props: PropertySelectorProps) => {
   }
   if (props.type === "ordinary") {
     return (
-      <div className="bg-secondary px-3 py-1 rounded-lg">
+      <div
+        className={`bg-secondary cursor-pointer px-3 py-1 rounded-lg  border-2 border-white ${
+          props.formData.selectedPropertyvalString ===
+            props.propertyval.propertyval._id &&
+          "border-2 border-white outline-2 outline-green-600"
+        }`}
+      >
         {props.propertyval.propertyval.value}
       </div>
     );
